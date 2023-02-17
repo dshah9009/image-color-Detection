eb29470c9bfe3f45f4fe255ebadc654d005f79c9
Deepak kumar shah <deepakshahwal999@gmail.com>
2023-02-17 19:36:08 +0530

    commiting 1 to 0
    branch1

--- python-project-color-detection/color_detection.py
+++ python-project-color-detection/color_detection.py
@@ -14,7 +14,7 @@ img = cv2.imread(img_path)
 
 #declaring global variables (are used later on)
 clicked = False
-r = g = b = xpos = ypos = 1
+r = g = b = xpos = ypos = 0
 
 #Reading csv file with pandas and giving names to each column
 index=["color","color_name","hex","R","G","B"]
